cc08c9c92e4a702d5c9c13a6fa301fcaca10f69e
WinXi <1057797031@qq.com>
2026-06-19 13:32:36 +0800

    2026.6.19第一次提交

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T,M;
+    cin>>T>>M;
+    unordered_map<int,int>cost,val;
+    for(int i=1;i<=M;++i){
+        int temp1,temp2;
+        cin>>temp1>>temp2;
+        cost[i]=temp1;
+        val[i]=temp2;
+    }
+    
+
+    
+    
+
+    return 0;
+}
